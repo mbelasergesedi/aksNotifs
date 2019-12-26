ecba91fb1290d92c2f923b5e77b743f48c21c109
Mbela Serge <mbelaserge@gmail.com>
2019-12-26 12:51:07 +0100

    Correct App Module
    Correct App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,6 @@ import { QryQuotationsService } from './services/quotation.services';
 import { QryStatNotifs } from './tab1/statnotif.service';
 import { ManageUsers } from './services/manageUsers.service';
 import { RoiService } from './services/roi.service';
-import { InfosService } from './services/information.service';
 // Captcha
 import { QrySignalementService } from './services/signalement.service';
 import { InteractionSearch } from './services/interaction.service';
@@ -90,7 +89,6 @@ import { NgHttpLoaderModule } from 'ng-http-loader';
     InteractionSearch,
     QryInteractionService,
     ResultatInteractionService,
-    InfosService,
     QrySignalementService,
     DciSearch,
     VilleService,
