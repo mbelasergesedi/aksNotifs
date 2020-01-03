00862318f6b2500dbdb52d2db0cfaaaa6f3d0adc
Mbela Serge <mbelaserge@gmail.com>
2020-01-03 03:22:21 +0100

    only notifs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NotificationsService } from './notification.service';
 import { map } from 'rxjs/operators';
 import { AuthService } from './../services/AuthService';
@@ -14,166 +14,36 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: './tab1.page.html',
   styleUrls: ['./tab1.page.scss']
 })
-export class Tab1Page implements OnInit {
+export class Tab1Page {
   pushes: any = [];
-  // Nouvelles
-  ObjNouvelles = {
-    categorie: 'Nouvelles',
-    maxdata: 10
-  };
-  // Alertes
-  ObjAlertes = {
-    categorie: 'Alertes',
-    maxdata: 10
-  };
-
-  // Ordre des pharmaciens
-  ObjOpharmaciens = {
-    categorie: 'Opharmaciens',
-    maxdata: 10
-  };
-  // Fermeture pharmacie
-  ObjClosePharma = {
-    categorie: 'closePharma',
-    maxdata: 10
-  };
-  //   Retrait produit
-  ObjRetiredPharma = {
-    categorie: 'RetiredPharma',
-    maxdata: 10
-  };
-  //   Ordre des médecins
-  ObjOrdMedecins = {
-    categorie: 'Omedecins',
-    maxdata: 10
-  };
-  //   Ordre des dentistes
-  ObjOrdDentistes = {
-    categorie: 'ODentistes',
-    maxdata: 10
-  };
-  //   Ordre des Kine
-  ObjOrdKine = {
-    categorie: 'OKine',
-    maxdata: 10
-  };
-  //   Ordre tradi
-  ObjOrdTradi = {
-    categorie: 'OTradi',
-    maxdata: 10
-  };
-  //   Ordre infis
-  ObjOrdInfis = {
-    categorie: 'OInfis',
-    maxdata: 10
-  };
-  status = 'ONLINE';
-  isConnected = true;
-  interactions: any;
-  rois: any;
-  stat: any;
-  userEmail: string;
-  userName: string;
-  startAt = new Subject();
-  endAt = new Subject();
-  navCtrl: any;
-  logged: any;
-  medecin: any;
-  name: any;
-  Appversion: any;
-  subscribedParam: any;
-  constructor(
-    public notificationService: NotificationsService,
-    public authService: AuthService,
-    private fcm: FCM,
-    private appVersion: AppVersion,
-    private roiService: RoiService,
-    private statusBar: StatusBar,
-    public plt: Platform,
-    public connectionService: ConnectionService
-  ) {
-    this.connectionService.monitor().subscribe(isConnected => {
-      this.isConnected = isConnected;
-      if (this.isConnected) {
-        this.status = 'ONLINE';
-      } else {
-        this.status = 'OFFLINE';
-      }
-    });
-  }
-
-  ngOnInit() {
-    this.statusBar.overlaysWebView(true);
-    this.appVersion.getAppName();
-    this.appVersion.getPackageName();
-    this.appVersion.getVersionCode();
-    this.appVersion.getVersionNumber();
-    this.Appversion = this.appVersion.getVersionNumber();
-    // tslint:disable-next-line: no-unused-expression
-    this.userEmail;
-    // tslint:disable-next-line: no-unused-expression
-    this.medecin;
-    // tslint:disable-next-line: no-unused-expression
-    this.logged;
-    if (this.authService.userDetails()) {
-      this.userEmail = this.authService.userDetails().email;
-      this.logged = true;
-      this.userName = this.authService.userDetails().displayName;
-    } else {
-      this.logged = false;
-    }
-    // Notifications Service
-    this.notificationService
-      .getNotification()
-      .snapshotChanges()
-      .pipe(
-        map(changes =>
-          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
-        )
-      )
-      .subscribe(interactions => {
-        this.interactions = interactions;
-      });
-    // Roi
-    this.roiService
-      .getRoiList()
-      .snapshotChanges()
-      .pipe(
-        map(changes =>
-          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
-        )
-      )
-      .subscribe(rois => {
-        this.rois = rois;
-      });
-
+  constructor(private fcm: FCM, public plt: Platform) {
     this.plt.ready()
       .then(() => {
         this.fcm.onNotification().subscribe(data => {
           if (data.wasTapped) {
-            console.log(data);
-            console.log('Received in background');
+            console.log(data)
+            console.log("Received in background");
             this.pushes.push({
               body: data.body,
               title: data.title
-            });
-            console.log(this.pushes);
+            })
+            console.log(this.pushes)
           } else {
-            console.log(data);
-            console.log('Received in foreground');
+            console.log(data)
+            console.log("Received in foreground");
             this.pushes.push({
               body: data.body,
               title: data.title
-            });
-            console.log(this.pushes);
-          }
+            })
+            console.log(this.pushes)
+          };
         });
 
         this.fcm.onTokenRefresh().subscribe(token => {
           // Register your new token in your back-end if you want
           // backend.registerToken(token);
         });
-      });
+      })
   }
   subscribeToTopic() {
     this.fcm.subscribeToTopic('enappd');
