c865136cc8c9ff24e08410699a65c796b60c45b0
mbelasergesedi <mbelaserge@aksantimed.com>
2020-01-16 17:39:12 +0100

    Revert "ChangeForm"
    This reverts commit e8feb96db89043e50f2ade1429945de6a1caf265.

--- src/app/tab3/accountStudent/account.component.ts
+++ src/app/tab3/accountStudent/account.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { AuthenticateService } from '../../services/authentication.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SchoolsService } from '../../services/school.service';
 import { VilleService } from '../../services/city.service';
 import { QryCustomerService } from '../../services/customers.service';
+
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Customers } from '../../model/customers.model';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { ToastController } from '@ionic/angular';
-import { UniqueDeviceID } from '@ionic-native/unique-device-id/ngx';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -20,7 +19,9 @@ import 'firebase/firestore';
 export class AccountComponent implements OnInit {
   // tslint:disable-next-line: variable-name
   account_form: FormGroup;
+
   errorMessage = '';
+
   // tslint:disable-next-line: variable-name
   validation_messages = {
     email: [
@@ -33,12 +34,7 @@ export class AccountComponent implements OnInit {
     ]
   };
   enr: any;
-  uuid: any;
-  iddevice: any;
-  model: any;
-  platform: any;
-  manufacturer: any;
-  version: any;
+  school: any;
   itemCollection: any;
   MyData: any;
   items: Observable<[any]>;
@@ -46,28 +42,20 @@ export class AccountComponent implements OnInit {
   [x: string]: any;
   list: Customers[];
   signupForm: FormGroup;
-  ville: any;
   successMessage: string;
+
   constructor(private formBuilder: FormBuilder,
               private authenticateService: AuthenticateService,
+              private schoolsService: SchoolsService,
               private villeService: VilleService,
-              private geolocation: Geolocation,
-              private uniqueDeviceID: UniqueDeviceID,
               private db: AngularFirestore,
               private qryCustomerService: QryCustomerService,
-              private toastController: ToastController) {
-  }
-  async logInToast() {
-    const toast = await this.toastController.create({
-      position: 'middle',
-      message: 'Vous êtes deconnecté.',
-      duration: 2000
-    });
-    toast.present();
-  }
+              private router: Router) { }
+
   ngOnInit() {
     this.initForm();
     this.getVille();
+    this.getSchool();
   }
   getVille() {
     this.villeService.getVille().snapshotChanges().pipe(
@@ -79,34 +67,33 @@ export class AccountComponent implements OnInit {
     ).subscribe(ville => {
       this.villeRef = ville;
       this.ville = ville;
+     // console.log(this.ville);
+    });
+  }
+
+
+  getSchool() {
+    this.schoolsService.getSchool().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(school => {
+      this.schoolRef = school;
+      this.school = school;
+      // console.log(this.school);
     });
   }
   initForm() {
     // Validations patterns
-    // Validations patterns
     this.account_form = this.formBuilder.group({
-      civilite: new FormControl('', Validators.compose([
-        Validators.required
-      ])),
       nom: new FormControl('', Validators.compose([
         Validators.required
       ])),
       prenom: new FormControl('', Validators.compose([
         Validators.required
       ])),
-      profession: new FormControl('', Validators.compose([
-        Validators.required
-      ])),
-      ville: new FormControl('', Validators.compose([
-        Validators.required
-      ])),
-      structure: new FormControl('', Validators.compose([
-        Validators.required
-      ])),
-      device: [],
-      version: [],
-      model: [],
-      manufacturer: [],
       email: new FormControl('', Validators.compose([
         Validators.required,
         Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
@@ -115,7 +102,27 @@ export class AccountComponent implements OnInit {
         Validators.minLength(5),
         Validators.required
       ])),
+      structure: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      device: new FormControl('', Validators.compose([
+      ])),
+      geocoding: new FormControl('', Validators.compose([
+      ])),
+
+      ville: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      ecole: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
       validate: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      profession: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      civilite: new FormControl('', Validators.compose([
         Validators.required
       ])),
       gsm: new FormControl('', Validators.compose([
@@ -127,61 +134,27 @@ export class AccountComponent implements OnInit {
   tryRegister() {
     const data = this.account_form.value;
     // 1. On cherche si l'email de l'utilisateur est déjà présent dans la DB;
-    this.itemCollection = this.db.collection<any[]>('customers', ref => ref.where('data.email', '==',
+    this.itemCollection = this.db.collection<any[]>('customers', ref => ref.where('email', '==',
       this.account_form.get('email').value));
-    this.items = this.itemCollection.valueChanges().subscribe(async (val: any) => {
+    this.items = this.itemCollection.valueChanges().subscribe((val: any) => {
       this.enregistrement = val;
-     // console.log(this.enregistrement);
-     // console.log(this.enregistrement.length);
+
       // 2. S'il n'est pas présent, on l'inscrit dans la DB et dans système d'authentification;
-      if (this.enregistrement && this.enregistrement.constructor === Array && this.enregistrement.length === 0) {
-        this.uniqueDeviceID.get()
-          .then((uuid: any) => this.uuid = uuid)
-          .catch((error: any) => this.error = error);
-        this.uniqueDeviceID.get()
-          .then((model: any) => this.model = model)
-          .catch((error: any) => this.error = error);
-        this.uniqueDeviceID.get()
-          .then((version: any) => this.version = version)
-          .catch((error: any) => this.error = error);
-
-        this.uniqueDeviceID.get()
-          .then((manufacturer: any) => this.manufacturer = manufacturer)
-          .catch((error: any) => this.error = error);
-
-        this.qryCustomerService.createCustomer({ data, createdAt: firebase.firestore.FieldValue.serverTimestamp() });
+      if (Object.keys(this.enregistrement).length === 0) {
+        this.qryCustomerService.createCustomer(data);
         this.authenticateService.registerUser(this.account_form.get('email').value, this.account_form.get('password').value)
-          .then(() => {
+          .then(res => {
+           //  console.log(res);
             this.errorMessage = '';
             this.successMessage = 'Votre compte a été crée.';
-           // console.log( this.successMessage);
-           // console.log( this.account_form.get('email').value);
-            // console.log( this.account_form.get('password').value);
           }, err => {
             // console.log(err);
             this.errorMessage = err.message;
             this.successMessage = '';
           });
         // 3. Sinon on le previent qu'il est déjà présent dans la DB;
-        // } //else {
-        // console.log('Utilisateur déjà présent');
-        const toast = this.toastController.create({
-         message: 'Félicitations ! Votre compte a été crée.',
-         position: 'middle',
-          duration: 2000
-        });
-        (await toast).present();
-        // }
-      }
-      // tslint:disable-next-line: max-line-length
-      if (this.enregistrement && this.enregistrement.constructor === Array && this.enregistrement.length !== 0) {
-         const toast = this.toastController.create({
-        message: 'Désolé ce compte existe déjà et ne peut être dupliqué.',
-        position: 'middle',
-         duration: 2000
-       });
-         (await toast).present();
-       }
-    });
+      } else { console.log('Utilisateur déjà présent'); }
+    }
+    );
   }
 }
